96f6bf924a4391296cdca1662954f8604d214cfa
aergid <develer@gmail.com>
2024-01-05 17:32:20 +0100

    Initial workig nix-darwin system after bootstrap from template

---
+++ modules/darwin/casks.nix
@@ -0,0 +1,32 @@
+_:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+  "insomnia"
+  "ngrok"
+  "postico"
+  "keepassxc"
+
+  # Communication Tools
+  "discord"
+  "notion"
+  "slack"
+  "telegram"
+
+  # Utility Tools
+  "appcleaner"
+  "syncthing"
+
+  # Entertainment Tools
+  "vlc"
+
+  # Productivity Tools
+  "raycast"
+  "asana"
+
+  # Browsers
+  "firefox"
+
+  # AI
+]
