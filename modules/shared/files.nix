a6219a494b1fa3a99cc4be647e1fd488e844634e
aergid <develer@gmail.com>
2024-01-26 10:11:25 +0100

    Add intelij idea extra config files

--- modules/shared/files.nix
+++ modules/shared/files.nix
@@ -1,4 +1,10 @@
 { pkgs, config, ... }:
 {
+  ".ideavimrc" = {
+    source = ../../files/shared/.ideavimrc;
+  };
+  "idea-project.xml" = {
+    source = ../../files/shared/idea-project.xml;
+  };
 
 }
