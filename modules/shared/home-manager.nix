b20bfbeec8a904f561ddcb577a13ce7a34d9e62e
aergid <develer@gmail.com>
2024-01-30 09:48:03 +0100

    Add fish prompt hint when runing from vifm

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -189,6 +189,13 @@ let name = "aergid";
     enable = true;
     interactiveShellInit = ''
       set fish_greeting # Disable greeting
+      functions -c fish_prompt _old_fish_prompt
+      function fish_prompt
+	if set -q VIFM
+	  echo -n -s (set_color -b blue white) "(VIFM)" (set_color normal) " "
+	end
+	_old_fish_prompt
+      end
     '';
     shellAliases = {
       gc = "git commit";
