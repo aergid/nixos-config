d9d71ef6c11fc44bc2334d54603849b1d6e162a6
aergid <develer@gmail.com>
2024-10-26 22:16:59 +0200

    Tune barbar plugin

--- modules/shared/nixvim/plugins/barbar.nix
+++ modules/shared/nixvim/plugins/barbar.nix
@@ -1,15 +1,41 @@
 {
   programs.nixvim.plugins.barbar = {
     enable = true;
-    settings = {
-      focus_on_close = "left";
-      sidebar_filetypes.nvimTree = true;
+
+    # TODO
+    # Integate with one of session plugins
+    # https://github.com/romgrk/barbar.nvim?tab=readme-ov-file#features
+
+    keymaps = {
+      next.key = "<TAB>";
+      previous.key = "<S-TAB>";
+      close.key = "<C-x>";
+      restore.key = "<C-A-x>";
+      pick.key = "<C-A-b>";
+      closeAllButCurrent.key = "<leader>o";
     };
 
-    # keymaps = {
-      # next = "<TAB>";
-      # previous = "<S-TAB>";
-      # close = "<C-x>";
-    # };
+    settings = {
+      animation = true;
+      tabpages = true;
+      clickable = true;
+      highlight_alternate = true;
+      insert_at_end = true;
+      focus_on_close = "previous";
+      sidebar_filetypes = {
+         neo-tree = {event = "BufWipeout"; text = "neo-tree"; align = "left";};
+      };
+      exclude_ft = [
+        "oil"
+        "qf"
+        "fugitive"
+      ];
+      exclude_name = [
+      ];
+      icons = {
+        button ="";
+        separator_at_end = false;
+      };
+    };
   };
 }
