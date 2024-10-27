c071f866d0f649ed999a159f8c5109d00f34d65d
aergid <develer@gmail.com>
2024-10-27 22:37:06 +0100

    Add keymaps for neo-tree focus, reveal and unfold all

--- modules/shared/nixvim/plugins/neo-tree.nix
+++ modules/shared/nixvim/plugins/neo-tree.nix
@@ -7,15 +7,35 @@
         action = ":Neotree action=focus reveal toggle<CR>";
         options.silent = true;
       }
+      {
+        mode = "n";
+        key = "<leader>|";
+        action = ":Neotree reveal<CR>";
+        options.silent = true;
+      }
+      {
+        mode = "n";
+        key = "<leader>/";
+        action = ":Neotree focus<CR>";
+        options.silent = true;
+      }
     ];
+
 
     plugins.neo-tree = {
       enable = true;
 
+      # https://github.com/nvim-neo-tree/neo-tree.nvim/discussions/770#discussion-4894073
+      # extraOptions = {
+      # };
+      window.mappings = {
+        Z = "expand_all_nodes";
+      };
+
       closeIfLastWindow = true;
       window = {
         width = 30;
-        autoExpandWidth = true;
+        autoExpandWidth = false;
       };
     };
   };
