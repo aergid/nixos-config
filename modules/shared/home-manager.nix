29bfe3b807fe03b366da10f81aa17ba51b2f0777
aergid <develer@gmail.com>
2024-10-30 22:23:52 +0100

    Change alacritty theme to match vim catppuccin

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -144,31 +144,84 @@ let name = "aergid";
 
       colors = {
         primary = {
-          background = "0x1f2528";
-          foreground = "0xc0c5ce";
+          background = "#1e1e2e";
+          foreground = "#cdd6f4";
+          dim_foreground = "#7f849c";
+          bright_foreground = "#cdd6f4";
         };
 
         normal = {
-          black = "0x1f2528";
-          red = "0xec5f67";
-          green = "0x99c794";
-          yellow = "0xfac863";
-          blue = "0x6699cc";
-          magenta = "0xc594c5";
-          cyan = "0x5fb3b3";
-          white = "0xc0c5ce";
+          black = "#45475a";
+          red = "#f38ba8";
+          green = "#a6e3a1";
+          yellow = "#f9e2af";
+          blue = "#89b4fa";
+          magenta = "#f5c2e7";
+          cyan = "#94e2d5";
+          white = "#bac2de";
         };
 
         bright = {
-          black = "0x65737e";
-          red = "0xec5f67";
-          green = "0x99c794";
-          yellow = "0xfac863";
-          blue = "0x6699cc";
-          magenta = "0xc594c5";
-          cyan = "0x5fb3b3";
-          white = "0xd8dee9";
+          black = "#585b70";
+          red = "#f38ba8";
+          green = "#a6e3a1";
+          yellow = "#f9e2af";
+          blue = "#89b4fa";
+          magenta = "#f5c2e7";
+          cyan = "#94e2d5";
+          white = "#a6adc8";
         };
+
+        cursor = {
+          text = "#1e1e2e";
+          cursor = "#f5e0dc";
+        };
+
+        vi_mode_cursor = {
+          text = "#1e1e2e";
+          cursor = "#b4befe";
+        };
+
+        search.matches = {
+          foreground = "#1e1e2e";
+          background = "#a6adc8";
+        };
+
+        search.focused_match = {
+          foreground = "#1e1e2e";
+          background = "#a6e3a1";
+        };
+
+        footer_bar = {
+          foreground = "#1e1e2e";
+          background = "#a6adc8";
+        };
+
+        hints.start = {
+          foreground = "#1e1e2e";
+          background = "#f9e2af";
+        };
+
+        hints.end = {
+          foreground = "#1e1e2e";
+          background = "#a6adc8";
+        };
+
+        selection = {
+          text = "#1e1e2e";
+          background = "#f5e0dc";
+        };
+
+        indexed_colors = [
+          {
+            index = 16;
+            color = "#fab387";
+          }
+          {
+            index = 17;
+            color = "#f5e0dc";
+          }
+        ];
       };
     };
   };
